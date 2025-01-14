21f38206a01b78e13fd3e6540a75e45642932cb2
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-01-14 19:14:01 +0000

    complete exercise 1C

--- Exercise2/src/Exercise1.java
+++ Exercise2/src/Exercise1.java
@@ -1,4 +1,5 @@
 public class Exercise1 {
+    // Exercise 1B
     void printEvenUp(int num) {
         if (num <= 0) {
             return;         // If num is negative, terminate function
@@ -10,5 +11,23 @@ public class Exercise1 {
                 System.out.println(i);
             }
         }
+    }
+
+    // Exercise 2B
+    // Return the latin alphabet from let1 up to let2
+    String getABCFromTo(char let1, char let2) {
+        // Terminate function is either characters are no letters
+        if (!Character.isLetter(let1) || !Character.isLetter(let2)) {
+            return "";
+        }
+
+        StringBuilder alphabet = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        // Convert let1 and let2 to strings for indexOf method
+        String let1Str = String.valueOf(let1).toUpperCase();
+        String let2Str = String.valueOf(let2).toUpperCase();
+        int let1Index = alphabet.indexOf(let1Str);
+        int let2Index = alphabet.indexOf(let2Str);
+
+        return alphabet.substring(let1Index, let2Index + 1);
     }
 }
--- Exercise2/src/MainExercise1.java
+++ Exercise2/src/MainExercise1.java
@@ -3,5 +3,7 @@ public class MainExercise1 {
     public static void main(String[] args) {
         Exercise1 ex1 = new Exercise1();
         ex1.printEvenUp(100);
+        String alphabet = ex1.getABCFromTo('m', '!');
+        System.out.println(alphabet);
     }
 }
